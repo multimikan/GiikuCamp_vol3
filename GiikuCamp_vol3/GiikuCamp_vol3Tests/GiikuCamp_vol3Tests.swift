ad34fa1308c1df8f0923d375f034ce4d7234944e
multimikan <koki20041008@gmail.com>
2025-05-16 14:33:22 +0900

    add xcode prj

---
+++ GiikuCamp_vol3/GiikuCamp_vol3Tests/GiikuCamp_vol3Tests.swift
@@ -0,0 +1,17 @@
+//
+//  GiikuCamp_vol3Tests.swift
+//  GiikuCamp_vol3Tests
+//
+//  Created by tknooa on 2025/05/16.
+//
+
+import Testing
+@testable import GiikuCamp_vol3
+
+struct GiikuCamp_vol3Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
