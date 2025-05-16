5dfe5ecf3d1948730345ee57c5ee18ffe027a087
multimikan <koki20041008@gmail.com>
2025-05-16 14:46:08 +0900

    add xcode prj in main

---
+++ GiikuCamp_vol3/GiikuCamp_vol3/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GiikuCamp_vol3
+//
+//  Created by tknooa on 2025/05/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
