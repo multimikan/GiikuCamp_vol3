69a30f3586429ba7b1d91f793bf316bb1ec68f75
multimikan <koki20041008@gmail.com>
2025-05-17 21:32:18 +0900

    model,vmodelを作成。サンプルビューも作成。

---
+++ GiikuCamp_vol3/GiikuCamp_vol3/View/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  GiikuCamp_vol3
+//
+//  Created by tknooa on 2025/05/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        NavigationView {
+            ChatView()
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
