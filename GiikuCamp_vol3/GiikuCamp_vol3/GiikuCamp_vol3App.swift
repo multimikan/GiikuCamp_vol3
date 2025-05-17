db6fc393ef2c08f49ff3a0358ef39c2d4c610f95
multimikan <koki20041008@gmail.com>
2025-05-17 20:09:44 +0900

    Revert "write AppDelegate(firebase)"
    This reverts commit c8173de42b7a0ce2934fde4bbd8254ad8273b689.

--- GiikuCamp_vol3/GiikuCamp_vol3/GiikuCamp_vol3App.swift
+++ GiikuCamp_vol3/GiikuCamp_vol3/GiikuCamp_vol3App.swift
@@ -6,25 +6,12 @@
 //
 
 import SwiftUI
-import FirebaseCore
 
-struct FirebaseCloudMessagingPracticeApp: App {
-    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+@main
+struct GiikuCamp_vol3App: App {
     var body: some Scene {
         WindowGroup {
-            NavigationStack{
-                ContentView()
-            }
+            ContentView()
         }
     }
-}
-
-
-class AppDelegate: NSObject, UIApplicationDelegate {
-  func application(_ application: UIApplication,
-                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-    FirebaseApp.configure()
-
-    return true
-  }
 }
