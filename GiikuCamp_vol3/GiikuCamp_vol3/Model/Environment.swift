fd3abe10d511053964de2673e89033b461c2551f
multimikan <koki20041008@gmail.com>
2025-10-31 16:32:29 +0900

    create env

--- GiikuCamp_vol3/GiikuCamp_vol3/Model/Environment.swift
+++ GiikuCamp_vol3/GiikuCamp_vol3/Model/Environment.swift
@@ -2,5 +2,5 @@ import Foundation
 
 enum MyEnvironment {
     // OpenAI API Keyを設定してください
-    static let openAIAPIKey = "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"
-} 
+    static let openAIAPIKey = APIKEY //位野木教授配布のAPIキー、アプリを不特定多数のユーザーが使用の際は要相談。
+}
